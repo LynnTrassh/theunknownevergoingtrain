ae35fe7e27315a4e93923485ed4af64f7a96c1c1
Zittacheng <zittacheng@gmail.com>
2020-02-28 16:21:36 -0500

    Auto stash before rebase of "origin/master"

---
+++ Assets/Z/Script/UI/EnergyBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Z
+{
+    public class EnergyBar : MonoBehaviour {
+        public Image image;
+        public Vector2 Range;
+        public float CurrentValue;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SetValue(CurrentValue);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetValue(float Value)
+        {
+            CurrentValue = Value;
+            image.fillAmount = Range.x + Value * (Range.y - Range.x);
+        }
+    }
+}
---
+++ Assets/Z/Script/Upgrade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z
+{
+    public class Upgrade : MonoBehaviour {
+        public ObjectInfo Info;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public ObjectInfo GetInfo()
+        {
+            return Info;
+        }
+    }
+}
